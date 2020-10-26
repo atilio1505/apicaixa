7162685dcafe655d333e52d0e11b133ec33bd822
atilio1505 <43754365+atilio1505@users.noreply.github.com>
2020-10-26 09:07:31 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ function alterarDados (nomeLoteria, dados) {
     resultado = dados.resultado
   }
 
-  listaGanhadores = dados.listaRateioPremio[0]
+  listaGanhadores = dados.listaRateioPremio['NUMERODEGANHADORES']
 
   teste  = listaGanhadores['NUMERODEGANHADORES']
 
